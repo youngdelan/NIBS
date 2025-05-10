9addf7692c697830d6db84bd21918d81690a0e0c
youngdelan <ianwambua83@gmail.com>
2025-05-10 15:05:21 -0700

    testing

---
+++ python/intro.py
@@ -0,0 +1,30 @@
+#print("introduction to python")
+
+#variables
+
+eng = 70
+name ="john"
+grade = 'B'
+avg = 70.0
+Eng =60
+
+sum = eng + maths
+#print(f"my name is{name}")
+#print(eng)
+#functions
+
+
+def Add():
+print("This is the add function")
+
+Add()
+
+
+
+
+
+
+
+
+
+
